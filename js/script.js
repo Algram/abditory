5e10211fff65247dd2ef162be8735675fd5c13ce
Xuesong Zhang <xuesong@ahorngmbh.de>
2016-03-01 15:35:43 +0100

    Complete page Detail

--- js/script.js
+++ js/script.js
@@ -11,37 +11,15 @@ $(function(){
 	$('#switch-button-2').click(function() {
 		$('.content-wrapper dl').addClass('display-2 ').removeClass('display-1');
 	});
-
-
+	$('#button-chart-process').click(function() {
+		$('.chart-process').addClass('display-block').removeClass('display-none');
+		$('.content-export').addClass('display-none').removeClass('display-block');
+		$('#button-chart-process').addClass('display-none').removeClass('display-block');
+	});
+	$('#content-save').click(function() {
+		$('.chart-process').addClass('display-none').removeClass('display-block');
+		$('.content-export').addClass('display-block').removeClass('display-none');
+		$('#button-chart-process').addClass('display-block').removeClass('display-none');
+	});
 
 });
-
-// var c1 = document.getElementById("c1");
-// var parent = document.getElementById("p1");
-// c1.width = parent.offsetWidth - 40;
-// c1.height = parent.offsetHeight - 40;
-//
-// var data1 = {
-//   labels: ["M", "T", "W", "T", "F", "S", "S"],
-//   datasets: [{
-//     fillColor: "rgba(255,255,255,.1)",
-//     strokeColor: "rgba(255,255,255,1)",
-//     pointColor: "#D99157",
-//     pointStrokeColor: "rgba(255,255,255,1)",
-//     data: [150, 200, 235, 390, 290, 250, 250]
-//   }]
-// }
-//
-// var options1 = {
-//   scaleFontColor: "rgba(255,255,255,1)",
-//   scaleLineColor: "rgba(255,255,255,1)",
-//   pointLabelFontColor: "#ffffff",
-//   scaleGridLineColor: "transparent",
-//   bezierCurve: false,
-//   scaleOverride: true,
-//   scaleSteps: 5,
-//   scaleStepWidth: 100,
-//   scaleStartValue: 0
-// }
-//
-// new Chart(c1.getContext("2d")).Line(data1, options1)
